d0b9ac66d72328b58ac33c76f6ca90021ea19731
Sergey Popov <nskpsv@mail.ru>
2024-09-29 09:56:05 +0700

    add github pages deploy

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,7 @@ export default defineNuxtConfig({
   compatibilityDate: "2024-04-03",
   devtools: { enabled: false },
   app: {
-    baseURL: "",
+    baseURL: "https://nskpsv.github.io/vpn-ad/",
   },
   css: ["~/assets/scss/main.scss", "normalize.css"],
   vite: {
